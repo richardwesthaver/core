dad86eeeca0cb2889438c6d83a036e3a9a1afad9
ellis <ellis@rwest.io>
2023-10-22 22:50:11 -0400

    tenex and krypt imports

---
+++ rust/lib/tenex/ipapi/lib.rs
@@ -0,0 +1,33 @@
+use reqwest::Error;
+use log::trace;
+use reqwest::Client;
+use std::net::IpAddr;
+
+pub async fn my_ip_verbose() -> Result<serde_json::Value, Error> {
+  let echo_json = Client::new()
+    .get("https://ipwhois.app/json/")
+    .send()
+    .await?
+    .json()
+    .await?;
+
+  trace!("{:#?}", echo_json);
+  Ok(echo_json)
+}
+
+pub async fn my_ip() -> Result<IpAddr, Error> {
+  let res = Client::new()
+    .get("https://ipinfo.io/ip")
+    .send()
+    .await?
+    .text()
+    .await?;
+  trace!("{:#?}", res);
+  Ok(res.parse().unwrap())
+}
+
+pub async fn get_ip() -> Result<(), Error> {
+  let ip = my_ip().await?;
+  println!("PUBLIC_IP : {:#?}", ip);
+  Ok(())
+}
