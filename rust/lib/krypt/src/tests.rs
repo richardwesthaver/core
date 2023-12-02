ab27b21f90482e6d6497f215377e713d9d94c7bc
ellis <ellis@rwest.io>
2023-12-02 18:42:13 -0500

    krypt stuff

--- rust/lib/krypt/src/tests.rs
+++ rust/lib/krypt/src/tests.rs
@@ -1,14 +1,7 @@
-use crate::linux_keyutils::{KeyRing, KeyRingIdentifier};
+use super::*;
 use zeroize::Zeroizing;
-
 #[test]
-fn default_user_keyring() {
-  let ring =
-    KeyRing::from_special_id(KeyRingIdentifier::Session, false).unwrap();
-  ring.add_key("krypt_test_default", "hackme").unwrap();
-  let key = ring.search("krypt_test_default").unwrap();
-  let mut buf = Zeroizing::new([0u8; 2048]);
-  let len = key.read(&mut buf).unwrap();
-  assert_eq!("hackme", std::str::from_utf8(&buf[..len]).unwrap());
-  key.invalidate().unwrap();
+fn keyutils_default() {
+  default_ks().unwrap();
+  join_ss("test_krypt").unwrap();
 }
