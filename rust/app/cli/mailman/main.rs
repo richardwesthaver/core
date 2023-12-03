d128c836fb972457177afacae13c8e2087aa2d4f
ellis <ellis@rwest.io>
2023-12-02 23:36:04 -0500

    rust krypt

--- rust/app/cli/mailman/main.rs
+++ rust/app/cli/mailman/main.rs
@@ -34,46 +34,4 @@ enum Cmd {
 async fn main() {
   let args = Cli::parse();
   println!("{:?}", args.name);
-  let client = Client::new()
-    .credentials(("ellis", "2pldn"))
-    //    .credentials(Credentials::bearer("
-    // 3XBDZ39pSQfrthH14cIFilI9raBN65zC8UKxcL2rp1XotT16FiN8yJVaFkabKL1h"))
-    .connect("https://mail.compiler.company:8080")
-    .await
-    .unwrap();
-  println!(
-    "{:?}",
-    client
-      .email_query(
-        Filter::and([email::query::Filter::has_keyword("$draft")]).into(),
-        [email::query::Comparator::from()].into()
-      )
-      .await
-      .unwrap()
-      .take_ids()
-  );
-  // Query mailboxes to obtain Inbox and Trash folder id
-  let inbox_id = client
-    .mailbox_query(
-      mailbox::query::Filter::role(mailbox::Role::Inbox).into(),
-      None::<Vec<_>>,
-    )
-    .await
-    .unwrap()
-    .take_ids()
-    .pop()
-    .unwrap();
-  let trash_id = client
-    .mailbox_query(
-      mailbox::query::Filter::role(mailbox::Role::Trash).into(),
-      None::<Vec<_>>,
-    )
-    .await
-    .unwrap()
-    .take_ids()
-    .pop()
-    .unwrap();
-
-  println!("Inbox: {:?}", inbox_id);
-  println!("Trash: {:?}", trash_id);
 }
