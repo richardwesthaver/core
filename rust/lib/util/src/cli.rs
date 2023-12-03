540e5f34d17d2db35ed2b3a13fd601f5b78b1044
ellis <ellis@rwest.io>
2023-12-03 18:44:14 -0500

    rustup tomfoolery - installer and lib/dl

--- rust/lib/util/src/cli.rs
+++ rust/lib/util/src/cli.rs
@@ -3,3 +3,11 @@
 pub use clap_complete;
 pub use indicatif;
 pub use terminal_clipboard;
+
+pub fn log_level_str_from_cli(b: u8) -> &'static str {
+  match b {
+    0 => "info",
+    1 => "debug",
+    _ => "trace",
+  }
+}
