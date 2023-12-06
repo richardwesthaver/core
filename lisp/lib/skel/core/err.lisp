70c1faf0e82488bd9742550ecd299d892faa9830
ellis <ellis@rwest.io>
2023-12-05 23:12:32 -0500

    skel/core/mk

--- lisp/lib/skel/core/err.lisp
+++ lisp/lib/skel/core/err.lisp
@@ -1,9 +1,12 @@
 ;;; Conditions
 (defpackage :skel/core/err
-  (:use :cl :std/err :std/sxp))
+  (:use :cl :std/err :std/sxp)
+  (:export 
+   :skel-syntax-error :skel-fmt-error
+   :skel-compile-error))
 
 (in-package :skel/core/err)
 
 (define-condition skel-syntax-error (sxp-syntax-error) ())
-
 (define-condition skel-fmt-error (sxp-fmt-error) ())
+(define-condition skel-compile-error nil nil)
