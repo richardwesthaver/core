49d5750e602d31a9f9dca3b112f7cab3cf3c7637
ellis <ellis@rwest.io>
2023-12-09 02:33:24 -0500

    lisp/ffi/xkb

--- lisp/ffi/tree-sitter/tests.lisp
+++ lisp/ffi/tree-sitter/tests.lisp
@@ -9,7 +9,18 @@
 
 (load-tree-sitter)
 
-(deftest ts-basic ()
+(deftest ts-json ()
   (load-tree-sitter-json)
-  (let ((parser (ts-parser-new)))
-    (ts-parser-set-language parser (tree-sitter-json))))
+  (let ((parser (ts-parser-new))
+        (lang (tree-sitter-json)))
+    (is (= (ts-language-version lang) 14))
+    (is (ts-parser-set-language parser lang))))
+
+(deftest ts-rust ()
+  (load-tree-sitter-rust)
+  (let ((parser (ts-parser-new))
+        (lang (tree-sitter-rust)))
+    ;; (is (= (ts-language-version lang) 14))
+    (is (ts-parser-set-language parser lang))))
+
+
