b558d836cabbcf8f4ba7a69e02ba725577e40f2c
ellis <ellis@rwest.io>
2023-12-09 23:20:06 -0500

    packy updates

--- lisp/lib/packy/pkg.lisp
+++ lisp/lib/packy/pkg.lisp
@@ -1,4 +1,27 @@
 (defpackage :packy
-  (:use :cl :std :rdb))
+  (:use :cl :std :rdb)
+  (:export
+   :pk-pack
+   :pk-unpack
+   :pk-install
+   :pk-uninstall
+   :pk-update
+   :pk-push
+   :pk-pull
+   :pk-query
+   :pk-sync
+   :pk-build
+   :pk-source
+   :pk-package
+   :pk-bundle
+   :pk-dependency
+   :pk-registry
+   :pk-db
+   :list-packages
+   :push-package
+   :pull-package
+   :query-package
+   :sync-package
+   :update-package
+   :build-package))
 
-(in-package :packy)
