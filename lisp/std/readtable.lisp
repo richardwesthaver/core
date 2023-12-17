2293b0262ac80373422d26c75a9b7d93f6ab46d4
ellis <ellis@rwest.io>
2023-12-17 16:05:38 -0500

    curry compose reader macros and tests

--- lisp/std/readtable.lisp
+++ lisp/std/readtable.lisp
@@ -1,3 +1,11 @@
+;;; std/readtable.lisp --- The Standard Readtable
+
+;; This readtable is accessible to systems which depend on the STD
+;; package.
+
+;;; Usage: (in-readtable :std)
+
+;;; Code:
 (in-package :std)
 
 (eval-when (:compile-toplevel :execute :load-toplevel)
@@ -5,7 +13,7 @@
     (declare (ignore sub-char))
     (unless numarg (setq numarg 1))
     `(lambda ,(loop for i from 1 to numarg
-                 collect (symb 'a i))
+                    collect (symb 'a i))
        ,(funcall
          (get-macro-character #\`) stream nil)))
 
@@ -48,44 +56,44 @@ $#
 
 ;; Nestable suggestion from Daniel Herring
 (eval-when (:compile-toplevel :load-toplevel :execute)
- (defun |#"-reader| (stream sub-char numarg)
-   (declare (ignore sub-char numarg))
-   (let (chars (state 'normal) (depth 1))
-     (loop do
-          (let ((curr (read-char stream)))
-            (cond ((eq state 'normal)
-                   (cond ((char= curr #\#)
-                          (push #\# chars)
-                          (setq state 'read-sharp))
-                         ((char= curr #\")
-                          (setq state 'read-quote))
-                         (t
-                          (push curr chars))))
-                  ((eq state 'read-sharp)
-                   (cond ((char= curr #\")
-                          (push #\" chars)
-                          (incf depth)
-                          (setq state 'normal))
-                         (t
-                          (push curr chars)
-                          (setq state 'normal))))
-                  ((eq state 'read-quote)
-                   (cond ((char= curr #\#)
-                          (decf depth)
-                          (if (zerop depth) (return))
-                          (push #\" chars)
-                          (push #\# chars)
-                          (setq state 'normal))
-                         (t
-                          (push #\" chars)
-                          (if (char= curr #\")
-                              (setq state 'read-quote)
-                              (progn
-                                (push curr chars)
-                                (setq state 'normal)))))))))
-     (coerce (nreverse chars) 'string))))
+  (defun |#"-reader| (stream sub-char numarg)
+    (declare (ignore sub-char numarg))
+    (let (chars (state 'normal) (depth 1))
+      (loop do
+        (let ((curr (read-char stream)))
+          (cond ((eq state 'normal)
+                 (cond ((char= curr #\#)
+                        (push #\# chars)
+                        (setq state 'read-sharp))
+                       ((char= curr #\")
+                        (setq state 'read-quote))
+                       (t
+                        (push curr chars))))
+                ((eq state 'read-sharp)
+                 (cond ((char= curr #\")
+                        (push #\" chars)
+                        (incf depth)
+                        (setq state 'normal))
+                       (t
+                        (push curr chars)
+                        (setq state 'normal))))
+                ((eq state 'read-quote)
+                 (cond ((char= curr #\#)
+                        (decf depth)
+                        (if (zerop depth) (return))
+                        (push #\" chars)
+                        (push #\# chars)
+                        (setq state 'normal))
+                       (t
+                        (push #\" chars)
+                        (if (char= curr #\")
+                            (setq state 'read-quote)
+                            (progn
+                              (push curr chars)
+                              (setq state 'normal)))))))))
+      (coerce (nreverse chars) 'string))))
 
-; This version is from Martin Dirichs
+                                        ; This version is from Martin Dirichs
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defun |#>-reader| (stream sub-char numarg)
     (declare (ignore sub-char numarg))
@@ -113,31 +121,31 @@ $#
             (nthcdr (length pattern) output))
            'string))))))
 
-; (set-dispatch-macro-character #\# #\> #'|#>-reader|)
+                                        ; (set-dispatch-macro-character #\# #\> #'|#>-reader|)
 
 (defun segment-reader (stream ch n)
   (if (> n 0)
-    (let ((chars))
-      (do ((curr (read-char stream)
-                 (read-char stream)))
-          ((char= ch curr))
-        (push curr chars))
-      (cons (coerce (nreverse chars) 'string)
-            (segment-reader stream ch (- n 1))))))
+      (let ((chars))
+        (do ((curr (read-char stream)
+                   (read-char stream)))
+            ((char= ch curr))
+          (push curr chars))
+        (cons (coerce (nreverse chars) 'string)
+              (segment-reader stream ch (- n 1))))))
 
 #+cl-ppcre
 (defmacro! match-mode-ppcre-lambda-form (o!args o!mods)
- ``(lambda (,',g!str)
-     (cl-ppcre:scan-to-strings
+  ``(lambda (,',g!str)
+      (cl-ppcre:scan-to-strings
        ,(if (zerop (length ,g!mods))
-          (car ,g!args)
-          (format nil "(?~a)~a" ,g!mods (car ,g!args)))
+            (car ,g!args)
+            (format nil "(?~a)~a" ,g!mods (car ,g!args)))
        ,',g!str)))
 
 #+cl-ppcre
 (defmacro! subst-mode-ppcre-lambda-form (o!args)
- ``(lambda (,',g!str)
-     (cl-ppcre:regex-replace-all
+  ``(lambda (,',g!str)
+      (cl-ppcre:regex-replace-all
        ,(car ,g!args)
        ,',g!str
        ,(cadr ,g!args))))
@@ -154,9 +162,9 @@ $#
                           (read-char stream)
                           1)
           (coerce (loop for c = (read-char stream)
-                     while (alpha-char-p c)
-                     collect c
-                     finally (unread-char c stream))
+                        while (alpha-char-p c)
+                        collect c
+                        finally (unread-char c stream))
                   'string)))
         ((char= mode-char #\s)
          (subst-mode-ppcre-lambda-form
@@ -167,12 +175,87 @@ $#
 
 ;; #+cl-ppcre (set-dispatch-macro-character #\# #\~ #'|#~-reader|)
 
+(defun lcurly-brace-reader (stream inchar)
+  (declare (ignore inchar))
+  (let ((spec (read-delimited-list #\} stream t)))
+    (if (typep (car spec) '(integer 0))
+        ;; Number of missing arguments
+        (let* ((n (pop spec))
+               (extra-args (loop repeat n collect (gensym "A"))))
+          (if (eq (cadr spec) '_)
+              (let ((provided-vars (loop repeat (length (cddr spec))
+                                         collect (gensym "P"))))
+                `(let ,(mapcar #'list provided-vars (cddr spec))
+                   (lambda ,extra-args (funcall (function ,(car spec))
+                                                ,@extra-args ,@provided-vars))))
+              (let ((provided-vars (loop repeat (length (cdr spec))
+                                         collect (gensym "P"))))
+                `(let ,(mapcar #'list provided-vars (cdr spec))
+                   (lambda ,extra-args (funcall (function ,(car spec))
+                                                ,@provided-vars ,@extra-args))))))
+        (if (eq (cadr spec) '_)
+            `(the (values function &optional) (rcurry (function ,(car spec)) ,@(cddr spec)))
+            `(the (values function &optional) (curry (function ,(car spec)) ,@(cdr spec)))))))
+
+(defun lsquare-brace-reader (stream inchar)
+  (declare (ignore inchar))
+  (list 'the '(values function &optional)
+        (cons 'compose (read-delimited-list #\] stream t))))
+
+(defun langle-quotation-reader (stream inchar)
+  (declare (ignore inchar))
+  (let ((contents (read-delimited-list #\» stream t))
+        (args (gensym "langle-quotation-reader")))
+    `(lambda (&rest ,args)
+       (,(car contents)                 ; Join function (or macro).
+        ,@(mapcar (lambda (fun) `(apply ,fun ,args)) (cdr contents))))))
+
+(defun lsingle-pointing-angle-quotation-mark-reader (stream inchar)
+  (declare (ignore inchar))
+  (flet ((function-p (form) (functionp (ignore-errors (eval form)))))
+    (let ((contents (read-delimited-list #\› stream t))
+          (arg (gensym "lsingle-pointing-angle-quotation-mark-reader")))
+      `(lambda (,arg)
+         (,(car contents)               ; Case form.
+          ,@(case (car contents)       ; If/when/unless guard.
+              ((if when unless)
+               `((funcall ,(cadr contents) ,arg)))
+              (cond nil)
+              (t (list arg)))
+          ,@(if (member (car contents) '(if when unless)) ; Clauses.
+                (mapcar (lambda (clause)
+                          (if (function-p clause)
+                              `(funcall ,clause ,arg)
+                              clause))
+                        (cddr contents))
+                (mapcar (lambda (clause)
+                          `(,(if (function-p (car clause))
+                                 `(funcall ,(car clause) ,arg)
+                                 (car clause))
+                            ,(if (function-p (cadr clause))
+                                 `(funcall ,(cadr clause) ,arg)
+                                 (cadr clause))))
+                        (cdr contents))))))))
+
 (eval-when (:compile-toplevel :load-toplevel :execute)
-(defreadtable :std
-  (:merge :modern)
-  (:dispatch-macro-char #\# #\" #'|#"-reader|)
-  (:dispatch-macro-char #\# #\> #'|#>-reader|)
-  #+cl-ppcre (:dispatch-macro-char #\# #\~ #'|#~-reader|)
-  (:dispatch-macro-char #\# #\` #'|#`-reader|)
-  (:dispatch-macro-char #\# #\f #'|#f-reader|)
-  (:dispatch-macro-char #\# #\$ #'|#$-reader|)))
+  (defreadtable :std
+    (:merge :modern)
+    ;; curry
+    (:macro-char #\{ #'std::lcurly-brace-reader)
+    (:macro-char #\} (get-macro-character #\) ))
+    (:macro-char #\[ #'std::lsquare-brace-reader)
+    (:macro-char #\] (get-macro-character #\) ))
+    (:macro-char #\« #'std::langle-quotation-reader)
+    (:macro-char #\» (get-macro-character #\) ))
+    (:macro-char #\‹ #'std::lsingle-pointing-angle-quotation-mark-reader)
+    (:macro-char #\› (get-macro-character #\) ))
+    ;; strings
+    (:dispatch-macro-char #\# #\" #'|#"-reader|)
+    (:dispatch-macro-char #\# #\> #'|#>-reader|)
+    ;; regex
+    (:dispatch-macro-char #\# #\~ #'|#~-reader|)
+    ;; lambdas
+    (:dispatch-macro-char #\# #\` #'|#`-reader|)
+    (:dispatch-macro-char #\# #\f #'|#f-reader|)
+    ;; shell
+    (:dispatch-macro-char #\# #\$ #'|#$-reader|)))
