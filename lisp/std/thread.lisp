671bdc68fdc82c31940d9199aefda75a8ec1438d
ellis <ellis@rwest.io>
2023-12-22 18:43:53 -0500

    castable added, still testing

--- lisp/std/thread.lisp
+++ lisp/std/thread.lisp
@@ -9,10 +9,6 @@
 ;;; Code:
 (in-package :std)
 
-(defvar *thread-id-map-lock* (make-mutex :name "thread-id-map"))
-
-(defvar *thread-id-map* (make-hash-table))
-
 (defun thread-support-p () (member :thread-support *features*))
 
 (defun print-thread-info (&optional (stream *standard-output*))
@@ -31,7 +27,7 @@
     nil))
 
 ;; this is all very unsafe. don't touch the finalizer thread plz.
-(defun find-thread (id)
+(defun find-thread-by-id (id)
   "Search for thread by ID which must be an u64. On success returns the thread itself or nil."
   (sb-thread::avlnode-data (sb-thread::avl-find id sb-thread::*all-threads*)))
 
@@ -49,11 +45,22 @@
                  (:constructor ,(symbolicate 'make- name))
                  (:conc-name "THREAD-")))))
 
-(defun make-threads (n name fn)
+(defun make-threads (n fn &key (name "thread"))
   (loop for i from 1 to n
         collect (make-thread fn :name (format nil "~A-~D" name i))))
 
-(defun timed-join-thread (thread &optional (timeout +timeout+))
+(defmacro with-threads ((idx n) &body body)
+  `(make-threads ,n (lambda (,idx) (declare (ignorable ,idx)) ,@body)))
+
+(defun finish-threads (&rest threads)
+  (let ((threads (flatten threads)))
+    (unwind-protect
+         (mapc #'join-thread threads)
+      (dolist (thread threads)
+        (when (thread-alive-p thread)
+          (destroy-thread thread))))))
+
+(defun timed-join-thread (thread timeout)
   (handler-case (sb-sys:with-deadline (:seconds timeout)
                   (join-thread thread :default :aborted))
     (sb-ext:timeout ()
@@ -66,3 +73,56 @@
   (when (thread-alive-p thread)
     (ignore-errors
       (terminate-thread thread))))
+
+;; from sb-thread
+(defun dump-thread ()
+  (let* ((primobj (sb-vm::primitive-object 'sb-vm::thread))
+         (slots (sb-vm::primitive-object-slots primobj))
+         (sap (current-thread-sap))
+         (thread-obj-len (sb-vm::primitive-object-length primobj))
+         (names (make-array thread-obj-len :initial-element "")))
+    (loop for slot across slots
+          do
+          (setf (aref names (sb-vm::slot-offset slot)) (sb-vm::slot-name slot)))
+    (flet ((safely-read (sap offset &aux (bits (sb-vm::sap-ref-word sap offset)))
+             (cond ((eql bits sb-vm:no-tls-value-marker) :no-tls-value)
+                   ((eql (logand bits sb-vm:widetag-mask) sb-vm:unbound-marker-widetag) :unbound)
+                   (t (sb-vm::sap-ref-lispobj sap offset))))
+           (show (sym val)
+             (let ((*print-right-margin* 128)
+                   (*print-lines* 4))
+               (format t " ~3d ~30a : ~s~%"
+                       #+sb-thread (ash sym (- sb-vm:word-shift))
+                       #-sb-thread 0
+                       #+sb-thread (sb-vm:symbol-from-tls-index sym)
+                       #-sb-thread sym
+                       val))))
+      (format t "~&TLS: (base=~x)~%" (sb-vm::sap-int sap))
+      (loop for tlsindex from sb-vm:n-word-bytes below
+            #+sb-thread (ash sb-vm::*free-tls-index* sb-vm:n-fixnum-tag-bits)
+            #-sb-thread (ash thread-obj-len sb-vm:word-shift)
+            by sb-vm:n-word-bytes
+            do
+         (unless (<= sb-vm::thread-allocator-histogram-slot
+                     (ash tlsindex (- sb-vm:word-shift))
+                     (1- sb-vm::thread-lisp-thread-slot))
+           (let ((thread-slot-name
+                  (if (< tlsindex (ash thread-obj-len sb-vm:word-shift))
+                           (aref names (ash tlsindex (- sb-vm:word-shift))))))
+                 (if (and thread-slot-name (sb-vm::neq thread-slot-name 'sb-vm::lisp-thread))
+                     (format t " ~3d ~30a : #x~x~%" (ash tlsindex (- sb-vm:word-shift))
+                             thread-slot-name (sb-vm::sap-ref-word sap tlsindex))
+                     (let ((val (safely-read sap tlsindex)))
+                       (unless (eq val :no-tls-value)
+                         (show tlsindex val)))))))
+      (let ((from (sb-vm::descriptor-sap sb-vm:*binding-stack-start*))
+            (to (sb-vm::binding-stack-pointer-sap)))
+        (format t "~%Binding stack: (depth ~d)~%"
+                (/ (sb-vm::sap- to from) (* sb-vm:binding-size sb-vm:n-word-bytes)))
+        (loop
+          (when (sb-vm::sap>= from to) (return))
+          (let ((val (safely-read from 0))
+                (sym #+sb-thread (sb-vm::sap-ref-word from sb-vm:n-word-bytes) ; a TLS index
+                     #-sb-thread (sb-vm::sap-ref-lispobj from sb-vm:n-word-bytes)))
+            (show sym val))
+          (setq from (sb-vm::sap+ from (* sb-vm:binding-size sb-vm:n-word-bytes))))))))
