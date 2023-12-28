4a24dfe8a5cb00cddf3fd5071c4fa647bfdfcad9
ellis <ellis@rwest.io>
2023-12-28 01:18:31 -0500

    introspection

--- lisp/lib/doc/pkg.lisp
+++ lisp/lib/doc/pkg.lisp
@@ -27,6 +27,52 @@
 (defpackage :doc
   (:use :cl :std :organ :sb-mop :sb-introspect)
   (:export
-   :doc-error))
+   :definition-specifier
+   :find-definitions
+   ;; err
+   :doc-error
+   ;; symbol
+   :do-symbol* :classify-symbol :symbol-classification-string))
 
 (in-package :doc)
+
+(defparameter *definition-types*
+  '(:variable defvar
+    :constant defconstant
+    :type deftype
+    :symbol-macro define-symbol-macro
+    :macro defmacro
+    :compiler-macro define-compiler-macro
+    :function defun
+    :generic-function defgeneric
+    :method defmethod
+    :setf-expander define-setf-expander
+    :structure defstruct
+    :condition define-condition
+    :class defclass
+    :method-combination define-method-combination
+    :package defpackage
+    :transform :deftransform
+    :optimizer :defoptimizer
+    :vop :define-vop
+    :source-transform :define-source-transform
+    :ir1-convert :def-ir1-translator
+    :declaration declaim
+    :alien-type :define-alien-type)
+  "Map SB-INTROSPECT definition type names to Slime-friendly forms")
+
+(defun definition-specifier (type)
+  "Return a pretty specifier for NAME representing a definition of type TYPE."
+  (getf *definition-types* type))
+
+(defun make-dspec (type name source-location)
+  (list* (definition-specifier type)
+         name
+         (sb-introspect::definition-source-description source-location)))
+
+(defun find-definitions (name)
+  (loop for type in *definition-types* by #'cddr
+        for defsrcs = (sb-introspect:find-definition-sources-by-name name type)
+        append (loop for defsrc in defsrcs 
+                     collect (list (make-dspec type name defsrc)
+                                   (sb-introspect:find-definition-sources-by-name name type)))))
