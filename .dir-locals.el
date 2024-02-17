54d2f35f89cfd61625b9b690a207761c6238877b
Richard Westhaver <ellis@rwest.io>
2024-02-17 18:41:47 -0500

    rm nu_plugin_rdb

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,6 @@
-((rust-mode . ((eglot-workspace-configuration
+((nil . ((indent-tabs-mode . nil)))
+ (rust-mode . ((eglot-workspace-configuration
                 . (:rust-analyzer (:cargo (:buildScripts (:enable t) (:features "all")))))))
  (nushell-mode . ((nushell-indent-offset . 2)))
- (nushell-ts-mode . ((nushell-indent-offset . 2))))
+ (nushell-ts-mode . ((nushell-indent-offset . 2)))
+ (makefile-mode . ((indent-tabs-mode . t))))
