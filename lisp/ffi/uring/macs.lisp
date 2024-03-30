9b745ba30f311f1447f6bac02d5fe00174a25214
Richard Westhaver <ellis@rwest.io>
2024-03-29 21:07:03 -0400

    opcodes

--- lisp/ffi/uring/macs.lisp
+++ lisp/ffi/uring/macs.lisp
@@ -12,3 +12,25 @@
 
 (defmacro def-with-ring (name &body args)
   `(defalien-int ,name (ring (* (struct io-uring))) ,@args))
+
+(defvar *io-opcodes* nil)
+
+;; io_uring_prep_*
+(defmacro def-io-op (val name slots &body builder)
+  "Define a wrapper for an io-uring opcode. This macro will create a
+structure class with NAME and SLOTS. BUILDER is the body of the BUILD
+method for this struct, with CONST bound to VAR."
+  (let ((struct-name (symbolicate "IO-OP-" name))
+        (const-name (symbolicate "+IO-" name "+"))
+        (alien-name (symbolicate "IORING-OP-" name)))
+    `(progn
+       (defconstant ,const-name ,val)
+       (defstruct ,struct-name ,@slots)
+       (defmethod build ((self ,struct-name) &key &allow-other-keys)
+         ,@builder)
+       (pushnew ',alien-name *io-opcodes*))))
+
+(defmacro def-alien-enum-with (name list)
+  `(define-alien-type ,name
+       (enum ,name
+             ,@(symbol-value list))))
