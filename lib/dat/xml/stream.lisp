86265b599913803b02834ca803c380bff98725b0
Richard Westhaver <ellis@rwest.io>
2026-03-10 19:51:10 -0400

    init xml/stream (klacks/sax)

---
+++ lib/dat/xml/stream.lisp
@@ -0,0 +1,12 @@
+;;; stream.lisp --- XML Stream support
+
+;; XML-STREAM
+
+;;; Commentary:
+
+;; inspired by CXML/KLACKS
+
+;; ref: https://cxml.common-lisp.dev/klacks.html
+
+;;; Code:
+;; (in-package :dat/xml)
