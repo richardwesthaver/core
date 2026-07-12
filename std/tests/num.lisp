629cd6f75dc4a2716d8f9540a4cb1374145960a3
Richard Westhaver <ellis@rwest.io>
2026-07-12 13:04:00 -0400

    rm perl-tests.lisp

--- std/tests/num.lisp
+++ std/tests/num.lisp
@@ -7,10 +7,10 @@
 (in-suite :std)
 
 (deftest leb128 ()
-  (loop for i from 0 below 1000
+  (loop for i in (iota 100 :step 100)
         do (is (= i (decode-uleb128 (encode-uleb128 i)))))
   (signals division-by-zero (decode-uleb128 (encode-uleb128 -1)))
-  (loop for i from -1000 below 1000
+  (loop for i in (iota 100 :start -1000 :step 1000)
         do (is (= i (decode-leb128 (encode-leb128 i))))
         do (is (= (* i i) (decode-leb128 (encode-leb128 (* i i)))))))
 
